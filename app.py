a3d04faae975cbedd5c3c37a04e41736c216b542
Lcicada <Lcicada>
2021-04-01 00:04:23 +0800

    add ddt+unpack+data

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ class Login():
 	_input_password_locator = (By.CSS_SELECTOR , 'input#pw.j-pwd.u-txt')
 	_login_button_locator = (By.CSS_SELECTOR, 'a.j-primary.u-btn2.u-btn2-2')
 
-	def __init__(self):
+	def __init__(self,url):
 		self.driver = webdriver.Chrome()
 		self.driver.implicitly_wait(4)
 		self.driver.maximize_window()
-		self.driver.get('https://music.163.com/')
+		self.driver.get(url)
 
 	def click_login(self):
 		self.driver.find_element(*self._login_locator).click()
@@ -31,11 +31,11 @@ class Login():
 	def phone_number_login(self):	
 		self.driver.find_element(*self._phone_number_login_locator).click()
 
-	def input_phone_number_account(self):
-		self.driver.find_element(*self._input_phone_number_locator).send_keys('165564464654')
+	def input_phone_number_account(self,phone_number):
+		self.driver.find_element(*self._input_phone_number_locator).send_keys(phone_number)
 	
-	def input_password(self):
-		self.driver.find_element(*self._input_password_locator).send_keys('253156454121')
+	def input_password(self,password):
+		self.driver.find_element(*self._input_password_locator).send_keys(password)
 	
 	def click_login_button(self):
 		self.driver.find_element(*self._login_button_locator).click()
@@ -43,14 +43,5 @@ class Login():
 	def close_window(self):
 		self.driver.quit()
 
-if __name__ == '__main__':
-	login = Login()
-	login.click_login()
-	login.select_other_login_mode()
-	login.check_privacy_policy_checkout()
-	login.phone_number_login()
-	login.input_phone_number_account()
-	login.input_password()
-	login.click_login_button()
-	login.close_window()
+
 
--- test_login.py
+++ test_login.py
@@ -1,19 +1,36 @@
 import unittest
 from app import Login
+from ddt import ddt,data,unpack
 
-
+@ddt
 class TestLogin(unittest.TestCase):
-
-    def test_login_wangyiyun(self):
-        login = Login()
+    
+    @data(('https://music.163.com/','165564464654','huudff'))
+    @unpack
+    def test_login_wangyiyun(self,url,phone_number,password):
+        login = Login(url)
         login.click_login()
         login.select_other_login_mode()
         login.check_privacy_policy_checkout()
         login.phone_number_login()
-        login.input_phone_number_account()
-        login.input_password()
+        login.input_phone_number_account(phone_number)
+        login.input_password(password)
         login.click_login_button()
         login.close_window()
 
 if __name__ =='__main__':
     unittest.main()
+
+
+# class TestLogin(unittest.TestCase):
+
+#     def test_login_wangyiyun(self):
+#         login = Login('https://music.163.com/')
+#         login.click_login()
+#         login.select_other_login_mode()
+#         login.check_privacy_policy_checkout()
+#         login.phone_number_login()
+#         login.input_phone_number_account('165564464654')
+#         login.input_password('hfied')
+#         login.click_login_button()
+#         login.close_window()
